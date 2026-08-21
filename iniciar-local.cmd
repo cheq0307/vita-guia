c36a596b2434405ffa7609b5bb59c8130d8db427
cheq0307 <cheq.0307@gmail.com>
2026-08-20 21:19:42 -0600

    chore: use portable Node launcher

--- iniciar-local.cmd
+++ iniciar-local.cmd
@@ -1,7 +1,9 @@
 @echo off
 setlocal
 cd /d "%~dp0"
+set "PATH=%~dp0tools\node;%PATH%"
 echo Iniciando Vita Guia en http://localhost:3000
 echo Asegurate de que MySQL este encendido en XAMPP.
-call npm run dev
+if not exist "node_modules\next" call "tools\node\npm.cmd" install
+call "tools\node\npm.cmd" run dev
 pause
